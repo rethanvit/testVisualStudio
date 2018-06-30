4f189daafa62351a10d3dcf3093684a86b389dea
Rethan Putta <rethanvit@gmail.com>
2018-06-30 18:03:29 -0500

    Added another hello world

--- test12/Program.cs
+++ test12/Program.cs
@@ -11,6 +11,7 @@ namespace test12
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
         }
     }
 }
